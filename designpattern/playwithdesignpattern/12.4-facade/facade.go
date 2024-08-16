7ab0a01918669d3587203c117a96f792e99b7247
Leo Gao <leo.gao@dbschenker.com>
2024-08-16 17:12:21 +0800

    add facade dp:add method

--- designpattern/playwithdesignpattern/12.4-facade/facade.go
+++ designpattern/playwithdesignpattern/12.4-facade/facade.go
@@ -4,17 +4,20 @@ type Facade struct {
 	one   *subSystemOne
 	two   *subSystemTwo
 	three *subSystemThree
+	four  *SubSystemFour
 }
 
 func NewFacade() *Facade {
 	one := &subSystemOne{}
 	two := &subSystemTwo{}
 	three := &subSystemThree{}
+	four := &SubSystemFour{}
 
 	return &Facade{
 		one:   one,
 		two:   two,
 		three: three,
+		four:  four,
 	}
 }
 
@@ -22,9 +25,11 @@ func (f *Facade) methodA() {
 	f.one.methodOne()
 	f.two.methodTwo()
 	f.three.methodThree()
+	f.four.methodFour()
 }
 
 func (f *Facade) methodB() {
 	f.two.methodTwo()
 	f.three.methodThree()
+
 }
