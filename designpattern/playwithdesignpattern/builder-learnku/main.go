3b9598aea52059d94e42fbb3b1da8eac2341647b
Leo Gao <leo.gao@dbschenker.com>
2024-08-17 16:05:57 +0800

    add builder dp:more graceful

--- designpattern/playwithdesignpattern/builder-learnku/main.go
+++ designpattern/playwithdesignpattern/builder-learnku/main.go
@@ -1,17 +1,17 @@
 package main
 
 func main() {
-	director := director{}
-	b1 := concreteBuilder1{product: &product{parts: make([]string, 0)}}
-	director.construct(&b1)
-	p1 := b1.getResult()
-	p1.show()
+	person := NewPersonBuilder().
+		WithName("LeoGao").
+		WithAge(20).
+		WithGender("Male").
+		WithAddress("中国南京").
+		Build()
 
-	b2 := concreteBuilder2{product: &product{parts: make([]string, 0)}}
-	director.construct(&b2)
-	p2 := b2.getResult()
-	p2.show()
+	person.Speak("精忠报国")
+	person.Sleep()
 }
 
+// 参考文档：
 // 这个列子更加的容易理解： https://learnku.com/docs/go-patterns/1.0.0/jian-zao-zhe-mo-shi-builder-pattern/14747
 // 这个列子更加的容易理解, 更加的优雅： https://github.com/ruanrunxue/Practice-Design-Pattern--Go-Implementation/blob/main/docs/go_practice_design_pattern__builder.md
