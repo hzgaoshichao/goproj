8406c16a4f2444dc045f2363584b7f071cff89c7
Leo Gao <leo.gao@dbschenker.com>
2024-08-24 03:11:51 +0800

    add composite dp

--- designpattern/playwithdesignpattern/14.4-observer/subject.go
+++ designpattern/playwithdesignpattern/14.4-observer/subject.go
@@ -21,7 +21,7 @@ func (s *Subject) attach(observer ObserverInterface) {
 
 func (s *Subject) detach(observer ObserverInterface) {
 	for i, ob := range s.observers {
-		//if observer == ob { // 这里涉及到机构体的比较, 需要特别注意 // 参考链接: https://segmentfault.com/a/1190000040099215
+		//if observer == ob { // 这里涉及到结构体的比较, 需要特别注意 // 参考链接: https://segmentfault.com/a/1190000040099215
 		if reflect.DeepEqual(observer, ob) {
 			s.observers = append(s.observers[:i], s.observers[i+1:]...)
 		}
